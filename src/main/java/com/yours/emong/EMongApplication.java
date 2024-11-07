454e34fdb436eb3878484278e90a4171488d037f
이예진 <140812985+316hui@users.noreply.github.com>
2024-11-07 21:30:33 +0900

    main

---
+++ src/main/java/com/yours/emong/EMongApplication.java
@@ -0,0 +1,13 @@
+package com.yours.emong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yours/emong/EMongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yours.emong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
