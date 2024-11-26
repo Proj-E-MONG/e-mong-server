c7a53339b4788df08e8d3156201c9db892cc1482
whitenoise13 <amnesiamar13@gmail.com>
2024-11-26 21:54:16 +0900

    Feat :: create NoticeController

--- src/main/java/com/yours/emong/domain/notice/controller/NoticeController.java
+++ src/main/java/com/yours/emong/domain/notice/controller/NoticeController.java
@@ -1,4 +1,34 @@
 package com.yours.emong.domain.notice.controller;
 
+
+import com.yours.emong.domain.notice.dto.NoticeCreateRequestDTO;
+import com.yours.emong.domain.notice.dto.NoticeUpdateRequestDTO;
+import com.yours.emong.domain.notice.entity.NoticeEntity;
+import com.yours.emong.domain.notice.service.NoticeService;
+import com.yours.emong.global.common.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chatrooms/{chatRoomId}/notices")
 public class NoticeController {
+
+    private final NoticeService noticeService;
+
+    // 공지 생성
+    @PostMapping
+    public ResponseEntity<NoticeEntity> createNotice(@RequestBody NoticeCreateRequestDTO requestDto) {
+        NoticeEntity notice = noticeService.createNotice(requestDto);
+        return new ResponseEntity<>(notice, HttpStatus.CREATED);
+    }
+
+    // 공지 수정
+    @PutMapping
+    public ResponseEntity<NoticeEntity> updateNotice(@RequestBody NoticeUpdateRequestDTO requestDto) {
+        NoticeEntity updatedNotice = noticeService.updateNotice(requestDto);
+        return new ResponseEntity<>(updatedNotice, HttpStatus.OK);
+    }
 }
