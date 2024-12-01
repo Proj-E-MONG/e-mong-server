3dfef1f38311972b503383f57091678eb131f494
whitenoise13 <amnesiamar13@gmail.com>
2024-12-01 15:43:39 +0900

    Feat :: fix NoticeRepository

--- src/main/java/com/yours/emong/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/yours/emong/domain/notice/repository/NoticeRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
-    List<NoticeEntity> findAllByChatRoomId(Long chatRoomId);
+    // 특정 채팅방의 공지 목록 조회
+    List<NoticeEntity> findByChatRoomId(Long chatRoomId);
+
+    // 특정 채팅방의 특정 공지 조회
+    Optional<NoticeEntity> findByNtcIdAndChatRoomId(Long noticeId, Long chatRoomId);  // 'ntcId'로 수정
 }
