19a210e68d5aa074067cd448dae57be63374a113
whitenoise13 <amnesiamar13@gmail.com>
2024-12-01 15:43:12 +0900

    Feat :: fix NoticeController

--- src/main/java/com/yours/emong/domain/notice/controller/NoticeController.java
+++ src/main/java/com/yours/emong/domain/notice/controller/NoticeController.java
@@ -2,6 +2,7 @@ package com.yours.emong.domain.notice.controller;
 
 
 import com.yours.emong.domain.notice.dto.NoticeCreateRequestDTO;
+import com.yours.emong.domain.notice.dto.NoticeResponseDTO;
 import com.yours.emong.domain.notice.dto.NoticeUpdateRequestDTO;
 import com.yours.emong.domain.notice.entity.NoticeEntity;
 import com.yours.emong.domain.notice.service.NoticeService;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,7 +31,28 @@ public class NoticeController {
     // 공지 수정
     @PutMapping
     public ResponseEntity<NoticeEntity> updateNotice(@PathVariable Long chatRoomId, @RequestBody NoticeUpdateRequestDTO requestDto) {
-        NoticeEntity updatedNotice = noticeService.updateNotice(requestDto);
+        NoticeEntity updatedNotice = noticeService.updateNotice(chatRoomId, requestDto); // chatRoomId 추가
         return new ResponseEntity<>(updatedNotice, HttpStatus.OK);
+    }
+
+    // 공지 삭제
+    @DeleteMapping("/{noticeId}")
+    public ResponseEntity<Void> deleteNotice(@PathVariable Long chatRoomId, @PathVariable Long noticeId) {
+        noticeService.deleteNotice(chatRoomId, noticeId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // 공지 목록 조회
+    @GetMapping
+    public ResponseEntity<List<NoticeResponseDTO>> getAllNotices(@PathVariable Long chatRoomId) {
+        List<NoticeResponseDTO> notices = noticeService.getAllNotices(chatRoomId);
+        return new ResponseEntity<>(notices, HttpStatus.OK);
+    }
+
+    // 공지 상세 조회
+    @GetMapping("/{noticeId}")
+    public ResponseEntity<NoticeResponseDTO> getNoticeDetail(@PathVariable Long chatRoomId, @PathVariable Long noticeId) {
+        NoticeResponseDTO notice = noticeService.getNoticeDetail(chatRoomId, noticeId);
+        return new ResponseEntity<>(notice, HttpStatus.OK);
     }
 }
